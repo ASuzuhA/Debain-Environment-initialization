25a9bf02e70f860101dc4f5bcb4fcb04e2b91756
ASuzuhA <124298064+ASuzuhA@users.noreply.github.com>
2024-11-04 02:23:28 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,12 +2,13 @@
 
 # 显示选项菜单
 echo "请选择要执行的操作:"
-select option in "允许系统以root形式进行SSH连接" "更改更新源并执行更新命令" "安装必要工具" "安装Docker并添加密钥及APT源" "退出"
+select option in "允许系统以root形式进行SSH连接" "更改更新源并执行更新命令" "安装必要工具" "退出"
 do
     case $REPLY in
         1)
             # 允许以root形式进行ssh连接
-            sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+            sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+            sed -i 's/#PermitRootLogin.*prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
             systemctl restart sshd
             echo "已允许系统以root形式进行SSH连接."
             ;;
@@ -15,15 +16,15 @@ do
 
             # 显示可选的更新源文件列表
             echo "可选的更新源文件:"
-            select source_file in "阿里云源.list" "清华大学源.list"
+            select source_file in "aliyun.list" "qinghua.list"
             do
                 case $REPLY in
                     1)
-                        cp 阿里云源.list /etc/apt/sources.list
+                        cp aliyun.list /etc/apt/sources.list
                         break
                         ;;
                     2)
-                        cp 清华大学源.list /etc/apt/sources.list
+                        cp qinghua.list /etc/apt/sources.list
                         break
                         ;;
                     *)
@@ -42,27 +43,6 @@ do
             echo "必要工具安装完成."
             ;;
         4)
-            # 安装Docker并添加密钥及APT源
-            read -p "是否要安装Docker并添加密钥及APT源? (y/n): " install_docker_apt
-            if [ "$install_docker_apt" = "y" ]; then
-                # 添加密钥
-                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-                echo "Docker密钥添加完成."
-
-                # 添加Docker的APT源
-                echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-ce.gpg] https://download.docker.com/linux/debian $(lsb_release -sc) stable" > /etc/apt/sources.list.d/docker.list
-                
-                # 安装Docker
-                apt-get update
-                apt-get install -y docker.io
-                systemctl start docker
-                systemctl enable docker
-                echo "Docker安装完成."
-            else
-                echo "未安装Docker及添加密钥及APT源."
-            fi
-            ;;
-        5)
             echo "退出脚本."
             break
             ;;
